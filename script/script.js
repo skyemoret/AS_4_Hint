9236acd9a672869e77ad45db95a4e74d0e5888ff
unknown <szhu@UTILE-T1700-11.Utile.local>
2015-04-02 12:32:22 -0400

    First commit

---
+++ script/script.js
@@ -0,0 +1,19 @@
+//Assignment 4
+//Due Thursday April 9
+
+var margin = {t:100,r:100,b:200,l:150},
+    width = $('.canvas').width() - margin.l - margin.r,
+    height = $('.canvas').height() - margin.t - margin.b;
+
+
+//Set up SVG drawing elements -- already done
+var svg = d3.select('.canvas')
+    .append('svg')
+    .attr('width', width + margin.l + margin.r)
+    .attr('height', height + margin.t + margin.b)
+    .append('g')
+    .attr('transform','translate('+margin.l+','+margin.t+')');
+
+//Create a mercator projection
+
+//Import data
