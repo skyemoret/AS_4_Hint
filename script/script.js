c385d0b1db7598f22966233a41a316a68c049834
siqister <siqi.zhu@gmail.com>
2015-04-09 17:07:10 -0400

    Assignment hint

--- script/script.js
+++ script/script.js
@@ -15,5 +15,82 @@ var svg = d3.select('.canvas')
     .attr('transform','translate('+margin.l+','+margin.t+')');
 
 //Create a mercator projection
+var projection = d3.geo.mercator()
+    .translate([width/2,height/2])
+    .scale(270);
+
+var scaleSize = d3.scale.sqrt().range([5,100]);
+
+//force layout
+var force = d3.layout.force()
+    .size([width,height])
+    .charge(0)
+    .gravity(0);
+
+
+d3.csv('data/world.csv',parse,function(err,world){
+    //console.log(world);
+
+    var extent = d3.extent(world, function(d){return d.pop;});
+    scaleSize
+        .domain(extent);
+
+
+    //First, use array.map to transform the original dataset --> "world"
+    var nodesArray = world.map(function(c){
+        //argument c is an element in the world array, which is a country
+        var xy = projection(c.lngLat);
+        return{
+            x:xy[0],
+            y:xy[1],
+            x0:xy[0],
+            y0:xy[1],
+            r: scaleSize(c.pop),
+            pop: c.pop,
+            id: c.id,
+            name: c.name
+        }
+    });
+
+
+    var countries = svg.selectAll('.country')
+        .data(nodesArray, function(d){ return d.id})
+        .enter()
+        .append('g')
+        .attr('class','country');
+
+    countries
+        .attr('transform',function(d){
+            return 'translate('+ d.x+','+ d.y+')';
+        });
+    countries
+        .append('circle')
+        .attr('r',function(d){
+            return scaleSize(d.pop);
+        });
+
+    force
+        .nodes(nodesArray)
+        .on('tick',onTick)
+        .start();
+
+    function onTick(e){
+        countries
+            .attr('transform',function(d){
+                return 'translate('+ d.x+','+ d.y+')';
+            });
+    }
+
+});
 
 //Import data
+function parse(d){
+    if(+d.UNc_longitude && +d.UNc_latitude){
+        return {
+            id: d.ISO3166A2,
+            name: d.ISOen_name,
+            pop: +d.population?+d.population:0,
+            lngLat: [+d.UNc_longitude, +d.UNc_latitude]
+        };
+    }
+}
